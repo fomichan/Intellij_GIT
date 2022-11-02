fb5e5b2ad29be2955a02aacdd9b533ba233f7556
Aleksei <fomich.alexey@yandex.ru>
2022-11-02 18:47:38 +0100

    First commit1

---
+++ src/com/fomich/lesson1/Homework1.java
@@ -0,0 +1,13 @@
+package com.fomich.lesson1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void method1(){
+        System.out.println("Homework1 решение!");
+    }
+}
+
+
