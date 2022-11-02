4f048add5c916ec6085c2145295ccea729702848
Aleksei <fomich.alexey@yandex.ru>
2022-11-02 19:08:33 +0100

    Homework 2

---
+++ src/com/fomich/lesson2/Homework2.java
@@ -0,0 +1,11 @@
+package com.fomich.lesson2;
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+    }
+
+    private void somemethod2(){
+        System.out.println("Lesson 2!");
+    }
+}
